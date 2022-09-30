f59c2d1fb838a0a40d1e51fe2b694581935748ed
Andrii Mudrak <andrey.mudrak.work@gmail.com>
2022-09-30 15:07:18 +0300

    Fix useCatImages hook

--- src/hooks/useCatImages.js
+++ src/hooks/useCatImages.js
@@ -1,14 +1,23 @@
-export function useCatImages(deps = []) {
-  let images = [];
+import { useEffect, useState } from 'react';
 
-  try {
-    const images = fetch(
-      "https://api.thecatapi.com/v1/images/search?limit=5"
-    ).then((res) => res.json());
-  } catch (e) {
-    console.error("fetch failed!");
-    console.error(e);
+export function useCatImages(deps = []) {
+  const [images, setImages] = useState([])
+
+  const fetchData = async () => {
+    try {
+      const result = await fetch(
+        "https://api.thecatapi.com/v1/images/search?limit=5"
+      ).then((res) => res.json());
+      setImages(result);
+    } catch (e) {
+      console.error("fetch failed!");
+      console.error(e);
+    }
   }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
 
   return images;
 }
